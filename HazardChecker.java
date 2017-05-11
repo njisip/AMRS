c30c2950976ae8507279e3dd9c5bfa9499860967
popejhonpaul <pgvillaro@up.edu.ph>
2017-05-11 13:39:58 +0800

    Add dependency checker

---
+++ HazardChecker.java
@@ -0,0 +1,45 @@
+import java.lang.String;
+import java.util.*;
+
+public class HazardChecker {
+
+	private HashMap<Integer,ArrayList<String>> instructions;
+	private int[][] depMatrix;
+	private int size;
+	private static final int NO_HAZARD = 0;
+	private static final int RAW = -1;
+	private static final int WAR = -2;
+	private static final int WAW = -3;
+	private static final int OP1 = 1;
+	private static final int OP2 = 2;
+
+
+	public HazardChecker(HashMap<Integer,ArrayList<String>> instructions) {
+		this.size = instructions.size();
+		this.instructions = instructions;
+		this.depMatrix = new int[size][size];
+	}
+
+	public void buildDepArray() {
+		for (int i=1; i<=this.size; i++) {	//iterates over all the instruction
+			for (int j=i+1; j<=size; j++) {			//iterates over all the instructions below the current instr
+				if(this.instructions.get(i).get(OP1).equals(instructions.get(j).get(OP1)))
+					this.depMatrix[i][j] = WAW;
+				else if(this.instructions.get(i).get(OP1).equals(instructions.get(j).get(OP2)))
+					this.depMatrix[i][j] = RAW;
+				else if (this.instructions.get(i).get(OP2).equals(instructions.get(j).get(OP1)))
+					this.depMatrix[i][j] = WAR;
+			}
+		}
+	}
+
+	public int checkTwoInstructions(int inst1, int inst2) {
+		//isnt1 < inst2
+		return this.depMatrix[inst1][inst2];
+	}
+
+	public int[][] getDepMatrix() {
+		return this.depMatrix;
+	}
+
+}
